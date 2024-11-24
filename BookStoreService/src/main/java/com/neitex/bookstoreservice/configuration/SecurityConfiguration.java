466e69017aacb41780e4db4c05b269145473cd0e
Pavel Matusevich <38156262+Neitex@users.noreply.github.com>
2024-11-24 17:10:15 +0300

    fix: enabled pre-post auth

--- BookStoreService/src/main/java/com/neitex/bookstoreservice/configuration/SecurityConfiguration.java
+++ BookStoreService/src/main/java/com/neitex/bookstoreservice/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import jakarta.servlet.DispatcherType;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @AllArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
   private final JwtAuthenticationFilter JwtAuthenticationFilter;
